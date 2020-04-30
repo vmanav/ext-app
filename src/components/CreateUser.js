e9092008d72b276a32370f2b3c2a1b42717be7cd
vmanav <zmanav.1999@gmail.com>
2020-04-30 20:58:45 +0530

    CreateUser Component done

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,7 +1,63 @@
 import React, { Component } from 'react';
 
 class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+        }
 
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        console.log("onChange Handler function INCOKED");
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+
+        const user = {
+            username: this.state.username,
+        }
+        console.log("New USER => ", user);
+
+        // WILLSAVE NEW USER HERE I GUESS
+
+        // NOTE : 
+        // Now, After someone has added a user,
+        // we will set the state to null again if someone eants to add more users
+        this.setState({
+            username: '',
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create New User</h3>
+                <form onSubmit={this.handleFormSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
 
 }
 
