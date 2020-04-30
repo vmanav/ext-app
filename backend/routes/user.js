949d501669d10cdd7d21890f64ab6f8c43583dba
vmanav <zmanav.1999@gmail.com>
2020-05-01 01:15:04 +0530

    Done woth ExerisesList Component

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 const User = require('../models/user');
 
 router.get('/', (req, res) => {
-    console.log("welcome to `users`");
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json("Error " + err))
 });
 
 router.post('/add', (req, res) => {
-    console.log("Agye POST pe .?");
+    // console.log("Agye POST pe .?");
+    // console.log("username from `body` = ", username);
     const username = req.body.username;
-    console.log("username from `body` = ", username);
 
     const newUser = new User({ username: username });
     newUser.save()
--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -1,12 +1,92 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+// Exercise Component
+const Exercise = (props) => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0, 10)}</td>
+        <td>
+            <Link to={"/edit/" + props.exercise._id}>edit</Link>
+            |
+            <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+        </td>
+    </tr>
+)
 
 class ExercisesList extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            exercises: []
+        }
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises')
+            .then((res) => {
+                console.log("res => ", res);
+                this.setState({
+                    exercises: res.data,
+                })
+
+                console.log("Updated STATE : ", this.state.exercises);
+            })
+            .catch((error) => {
+                console.log("Error : ", error);
+            })
+    }
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises' + id)
+            .then((res) => {
+                console.log(res.data);
+            })
+
+        this.setState({
+            exercises: this.state.exercises.filter(element => element._id !== id)
+        })
+    }
+
+    ExerciseListFunction() {
+        return (
+            this.state.exercises.map((exercise) => {
+                return (
+                    <Exercise
+                        exercise={exercise}
+                        deleteExercise={this.deleteExercise}
+                        key={exercise._id}
+                    />
+                )
+            })
+        )
+    }
 
     render() {
         return (
             <div>
-                <p>You are on the ExercisesList Component!s</p>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.ExerciseListFunction()}
+                    </tbody>
+                </table>
             </div>
         )
     }
